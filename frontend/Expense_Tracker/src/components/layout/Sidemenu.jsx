9415c9a4e366475e1ce3eb06d217a410b8cbe5d3
Pratham1854 <nagpalpratham@gmail.com>
2025-06-13 19:00:12 +0530

    working on side menu

--- frontend/Expense_Tracker/src/components/layout/Sidemenu.jsx
+++ frontend/Expense_Tracker/src/components/layout/Sidemenu.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
-
-const Sidemenu = () => {
+import React, { useContext } from 'react'
+import {SIDE_MENU_DATA} from '../../utils/data'
+import { UserContext } from '../../context/UserContext'
+import {useNavigate} from "react-router-dom"
+const Sidemenu = ({activemenu}) => {
+  const{user,clearuser}=useContext(UserContext)
+  const navigate=useNavigate();
+  const handleclick=(route)=>{
+    if(route==="logout"){
+      handlelogout();
+      return;
+    }
+    navigate(route);
+  };
+  const handlelogout=()=>{
+    localStorage.clear();
+    clearuser();
+    navigate("/login");
+  };
   return (
     <div>Sidemenu</div>
   )
